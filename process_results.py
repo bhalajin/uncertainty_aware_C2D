c0330cfc85644c7a674cb3fa4d977b1e64bdbed4
Ricardo Marques <ricardo.marques@ub.edu>
2021-11-10 21:40:10 +0100

    minor changes

--- process_results.py
+++ process_results.py
@@ -1,13 +1,40 @@
-with open('checkpoint/sym_0.2_weights_run2_cifar10_0.20_0.0_sym_acc.txt') as f:
-    best_acc   = 0;
-    best_epoch = -1;
-    for line in f:
-        str_test = line.split()
-        epoch    = int(str_test[0].split(":")[1])
-        accuracy = float(str_test[1].split(":")[1])
-        if accuracy >= best_acc:
-            best_acc   = accuracy
-            best_epoch = epoch
+import glob
+import argparse
+import numpy as np
 
-print("best_epoch: %d" % best_epoch)
-print("best_acc: %.2f" % best_acc)
+parser = argparse.ArgumentParser(description='Process Learning Results C2D + Weights')
+parser.add_argument('--noise', type=float, required=True)
+args   = parser.parse_args()
+
+noise_level = args.noise
+file_names  = './checkpoint/*%s*acc.txt' % str(noise_level)
+file_list   = glob.glob(file_names)
+
+print('\n')
+best_acc_list   = []
+final_acc_list  = []
+for i, f_n in enumerate(file_list):
+    print(f_n)
+    with open(f_n) as f:
+        best_acc   = 0;
+        best_epoch = -1;
+        for l, line in enumerate(f):
+            str_test = line.split()
+            epoch    = int(str_test[0].split(":")[1])
+            accuracy = float(str_test[1].split(":")[1])
+            if accuracy >= best_acc:
+                best_acc   = accuracy
+                best_epoch = epoch
+        final_acc  = accuracy
+        last_epoch = l
+    print("\tbest epoch: %d; best accuracy: %.2f" % (best_epoch, best_acc))
+    print("\tlast epoch: %d; last accuracy: %.2f\n" % (last_epoch, final_acc))
+
+    if last_epoch >= 359:
+        best_acc_list.append(best_acc)
+        final_acc_list.append(final_acc)
+
+print('Average Best Accuracy: %.3f' % np.average(best_acc_list))
+print('Standard Deviation Best Accuracy: %.3f\n' % np.std(best_acc_list))
+print('Average Last Accuracy: %.3f' % np.average(final_acc_list))
+print('Standard Deviation Last Accuracy: %.3f\n' % np.std(final_acc_list))
