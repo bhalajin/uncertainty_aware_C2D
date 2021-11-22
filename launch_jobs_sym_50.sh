acbc58d22c9d45a4b5ddcf267ccca0940fe73579
Ricardo Marques <ricardo.marques@ub.edu>
2021-11-22 09:50:37 +0100

    Partial results w-e 63

--- launch_jobs_sym_50.sh
+++ launch_jobs_sym_50.sh
@@ -4,8 +4,8 @@
 #
 #  ATTENTION!!! ADAPT CUDA VISIBLE DEVICE #
 #
-CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run1 --method selfsup --net resnet18
-CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run2 --method selfsup --net resnet18
-CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run3 --method selfsup --net resnet18
-CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run4 --method selfsup --net resnet18
-CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run5 --method selfsup --net resnet18
+CUDA_VISIBLE_DEVICES=1 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run1 --method selfsup --net resnet18
+CUDA_VISIBLE_DEVICES=1 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run2 --method selfsup --net resnet18
+CUDA_VISIBLE_DEVICES=1 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run3 --method selfsup --net resnet18
+CUDA_VISIBLE_DEVICES=1 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run4 --method selfsup --net resnet18
+CUDA_VISIBLE_DEVICES=1 python3 main_cifar.py --r 0.5 --noise_mode sym --lambda_u 150  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.5_weights_run5 --method selfsup --net resnet18
