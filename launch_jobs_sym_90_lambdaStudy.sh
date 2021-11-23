e0d3f84ea0924ba97601f77024bf18718024d73a
Ricardo Marques <ricardo.marques@ub.edu>
2021-11-23 10:53:30 +0100

    New Results 63

--- launch_jobs_sym_90_lambdaStudy.sh
+++ launch_jobs_sym_90_lambdaStudy.sh
@@ -18,8 +18,8 @@
 #CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.9 --noise_mode sym --lambda_u 550  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.9_weights_run1 --method selfsup --net resnet18
 
 # machine: 63, GPU0
-#CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.9 --noise_mode sym --lambda_u 700  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.9_weights_run1 --method selfsup --net resnet18
-#CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.9 --noise_mode sym --lambda_u 650  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.9_weights_run1 --method selfsup --net resnet18
+CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.9 --noise_mode sym --lambda_u 700  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.9_weights_run1 --method selfsup --net resnet18
+CUDA_VISIBLE_DEVICES=0 python3 main_cifar.py --r 0.9 --noise_mode sym --lambda_u 650  --dataset cifar100 --data_path ./../Datasets/CIFAR100  --experiment-name CF100_sym_0.9_weights_run1 --method selfsup --net resnet18
 
 
 # Results CIFAR-100 90% symetric noise (using our optimal lambda = 450)
